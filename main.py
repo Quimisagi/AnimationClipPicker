f90459020f2a9dae8f0f80644dd621f214acd804
Quimisagi <m5281007@u-aizu.ac.jp>
2025-01-23 10:34:14 +0900

    Added restart argument

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from video_processor import load_video_paths, process_video
 import argparse
 import sys
@@ -9,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description="Process videos in a directory")
     parser.add_argument("--parent_dir", type=str, required=True, help="Parent directory containing videos")
     parser.add_argument("--output_file", type=str, default='./accepted_videos.txt', help="Path of the output file to save the paths of accepted videos")
+    parser.add_argument("--restart", dest='restart', action='store_true', help="Restart the process from the beginning")
     args = parser.parse_args()
 
     parent_dir = args.parent_dir
@@ -18,51 +18,58 @@ def main():
         print("Error: No parent directory provided. Please pass --parent_dir argument.")
         sys.exit(1)  # Exit the program
 
-    # Check if the videos_path_file exists
+
+    if args.restart:
+        print("Restarting the process from the beginning.")
+        with open(videos_path_file, "w") as file:
+            file.write("")
+        with open(output_file, "w") as file:
+            file.write("")
+
     try:
+        # Attempt to read existing video paths from the file
         with open(videos_path_file, "r") as file:
             content = file.read().strip()
-            if content:
-                video_paths = content.splitlines()
-                print("Video paths loaded from file.")
-            else:
-                video_paths = load_video_paths(parent_dir)
-                with open(videos_path_file, "w") as file:
-                    file.write("\n".join(video_paths))
-                file.close()
-                print(f"Videos loaded from directory and saved to file {videos_path_file}")
-            
-    except FileNotFoundError:
+
+        if content:
+            video_paths = content.splitlines()
+            print("Video paths loaded from file.")
+        else:
+            raise ValueError("File is empty. Reloading video paths.")
+
+    except (FileNotFoundError, ValueError):
+        # Load video paths from the directory if the file doesn't exist or is empty
         video_paths = load_video_paths(parent_dir)
         with open(videos_path_file, "w") as file:
             file.write("\n".join(video_paths))
-        file.close()
         print(f"Videos loaded from directory and saved to file {videos_path_file}")
 
-    last_clip_number = 0
+    start_index = 0
 
-    try:
-        with open(output_file, "r") as file:
-            lines = file.readlines()
-            last_line = lines[-1] if lines else None
-            if last_line:
-                
-                parts = last_line.split(" - ")
-                if len(parts) > 0:
-                    last_clip_number = int(parts[0].strip())
-                    print(f"Extracted last clip: {last_clip_number}")
+
+    if not args.restart:
+        try:
+            with open(output_file, "r") as file:
+                lines = file.readlines()
+                last_line = lines[-1] if lines else None
+                if last_line:
+
+                    parts = last_line.split(" - ")
+                    if len(parts) > 0:
+                        start_index = int(parts[0].strip())
+                        print(f"Extracted last clip: {start_index}")
+                    else:
+                        print("No valid number found in the last line.")
                 else:
-                    print("No valid number found in the last line.")
-            else:
-                print("The file is empty or has no lines.")
-    except FileNotFoundError:
-        print("The file does not exist.")
+                    print("The file is empty or has no lines.")
+        except FileNotFoundError:
+            print("The file does not exist.")
 
     file = open(output_file, "a")
 
 
 
-    for i in range(last_clip_number, len(video_paths)):
+    for i in range(start_index, len(video_paths)):
         print(f"Processing video {i + 1}/{len(video_paths)}")
         video_path = video_paths[i]
         process_video(video_path, i, len(video_paths), file)
